46e6313b3c6d8241643be212b99a628d4f0a915e
Aman Chaudhary <agcy7320@gmail.com>
2020-02-06 20:39:38 +0530

    INITIAL-COMMIT

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('register/',views.register,name='register'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
+    path('profile/',views.profile,name='profile'),
+    path('tables/',views.tables,name='tables'),
+]
